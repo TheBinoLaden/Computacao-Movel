d53d885305380bc431bd5bcc44887c56e203505b
Rodrigo Albino <rodrigoalbino96@live.com.pt>
2022-10-09 02:14:45 +0100

    First Project - added some logic for the change of the colors and layout.

--- Projecto1/app/src/main/java/com/example/paint/Settings.java
+++ Projecto1/app/src/main/java/com/example/paint/Settings.java
@@ -2,13 +2,29 @@ package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
+    private Switch color_green;
+    private Switch dark_blue;
+    private Switch change_button_color;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        color_green = (Switch) findViewById(R.id.greenColor);
+        dark_blue = (Switch) findViewById(R.id.darker_blue);
+        change_button_color = (Switch) findViewById(R.id.change_color);
+
+        color_green.setOnClickListener(view -> {
+
+        });
     }
 }
