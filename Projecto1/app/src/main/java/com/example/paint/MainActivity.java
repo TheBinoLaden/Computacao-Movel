be7e524de1207411c245eff2e6b3c8b0ca6e7269
Rodrigo Albino <rodrigoalbino96@live.com.pt>
2022-10-09 21:43:22 +0100

    Second Assignment - refactor code, colors and new logic.

--- Projecto1/app/src/main/java/com/example/paint/MainActivity.java
+++ Projecto1/app/src/main/java/com/example/paint/MainActivity.java
@@ -1,26 +1,59 @@
 package com.example.paint;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import fragments.CanvasFragment;
+import fragments.PaletteFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button settings;
-    private Button about;
+    private Button settings, about, canvas, palette;
+
+    private static final String PROP_FILE = "app.properties";
+    private static final String COLOR_PROP = "color_background";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        settings = (Button) findViewById(R.id.settings);
+        try {
+            Properties properties = new Properties();
+            try (InputStream inputStream = getBaseContext().getAssets().open(PROP_FILE)) {
+                properties.load(inputStream);
+            }
+            final String value = properties.getProperty(COLOR_PROP);
+            findViewById(R.id.MainRelativeLayout).setBackgroundColor(Color.parseColor(value));
+        } catch (final IOException e) {
+            Log.e("ExceptionCaught", "error " + e.getMessage());
+            e.printStackTrace();
+        }
+        initializeAttributes();
+        setOnClick();
 
-        about = (Button) findViewById(R.id.about);
+    }
+
+    private void initializeAttributes() {
+        settings = findViewById(R.id.settings);
+        about = findViewById(R.id.about);
+        canvas = findViewById(R.id.canvas);
+        palette = findViewById(R.id.palette);
+    }
+
+    private void setOnClick() {
         settings.setOnClickListener(view -> {
             final Intent intent = new Intent(MainActivity.this, Settings.class);
             startActivity(intent);
@@ -31,5 +64,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        canvas.setOnClickListener(view -> replaceFragment(new CanvasFragment()));
+        palette.setOnClickListener(view -> replaceFragment(new PaletteFragment()));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- Projecto1/app/src/main/java/com/example/paint/Settings.java
+++ Projecto1/app/src/main/java/com/example/paint/Settings.java
@@ -2,31 +2,36 @@ package com.example.paint;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import utils.ResetUtils;
 
 public class Settings extends AppCompatActivity {
 
-    private SwitchCompat color_green;
-    private SwitchCompat dark_blue;
-    private SwitchCompat change_button_color;
+    private SwitchCompat colorGreen;
+    private SwitchCompat darkBlue;
+    private SwitchCompat changeButtonColor;
     private final List<SwitchCompat> switches = new ArrayList<>(3);
     private Button back;
     private final Map<SwitchCompat, LocalTime> switchTimeClicked = new HashMap<>(3);
+    private static final String PROP_FILE = "app.properties";
+    private static final String COLOR_PROP = "color_background";
 
 
     @SuppressLint("MissingInflatedId")
@@ -35,11 +40,25 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        initializeAttributes();
-        setOnClick();
+        try {
+            Properties properties = new Properties();
+            try (InputStream inputStream = getBaseContext().getAssets().open(PROP_FILE)) {
+                properties.load(inputStream);
+            }
+            final String value = properties.getProperty(COLOR_PROP);
+
+            findViewById(R.id.Settings).setBackgroundColor(Color.parseColor(value));
+            initializeAttributes();
+            setOnClick(properties);
+
+        } catch (final IOException e) {
+            Log.e("ExceptionCaught", "error " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 
-    public void changeColors(View view) {
+    public void changeColors(final Properties properties) {
 
         for (SwitchCompat switchCompat : switches) {
             if (switchCompat.isChecked()) {
@@ -49,44 +68,53 @@ public class Settings extends AppCompatActivity {
 
             }
         }
-        if (color_green.isChecked() && dark_blue.isChecked()) {
+        if (colorGreen.isChecked() && darkBlue.isChecked()) {
             Log.e("BothChecked", "Both buttons are checked, reverting to default.");
             ResetUtils.validateLastChecked(
-                    Arrays.asList(color_green, dark_blue),
-                    Arrays.asList(switchTimeClicked.get(color_green),
-                            switchTimeClicked.get(dark_blue)));
+                    Arrays.asList(colorGreen, darkBlue),
+                    Arrays.asList(switchTimeClicked.get(colorGreen),
+                            switchTimeClicked.get(darkBlue)));
+            properties.setProperty(COLOR_PROP, "#7DB0D8");
+            findViewById(R.id.Settings).setBackgroundColor(Color.parseColor("#7DB0D8"));
+        } else {
+            final String value = properties.getProperty(COLOR_PROP);
+            findViewById(R.id.Settings).setBackgroundColor(Color.parseColor(value));
         }
     }
 
     private void initializeAttributes() {
-        color_green = findViewById(R.id.greenColor);
-        dark_blue = findViewById(R.id.darker_blue);
-        change_button_color = findViewById(R.id.change_color);
+        colorGreen = findViewById(R.id.greenColor);
+        darkBlue = findViewById(R.id.darker_blue);
+        changeButtonColor = findViewById(R.id.change_color);
         back = findViewById(R.id.back_settings);
         populateList();
         populateMap();
     }
 
     private void populateList() {
-        switches.add(color_green);
-        switches.add(dark_blue);
-        switches.add(change_button_color);
+        switches.add(colorGreen);
+        switches.add(darkBlue);
+        switches.add(changeButtonColor);
     }
 
     private void populateMap() {
-        switchTimeClicked.put(color_green, null);
-        switchTimeClicked.put(dark_blue, null);
-        switchTimeClicked.put(change_button_color, null);
+        switchTimeClicked.put(colorGreen, null);
+        switchTimeClicked.put(darkBlue, null);
+        switchTimeClicked.put(changeButtonColor, null);
     }
 
-    private void setOnClick() {
-        color_green.setOnClickListener(view -> {
-            switchTimeClicked.replace(color_green, LocalTime.now());
-            changeColors(view);
+    private void setOnClick(final Properties properties) {
+
+
+        colorGreen.setOnClickListener(view -> {
+            switchTimeClicked.replace(colorGreen, LocalTime.now());
+            properties.setProperty(COLOR_PROP, "#52BE80");
+            changeColors(properties);
         });
-        dark_blue.setOnClickListener(view -> {
-            switchTimeClicked.replace(dark_blue, LocalTime.now());
-            changeColors(view);
+        darkBlue.setOnClickListener(view -> {
+            switchTimeClicked.replace(darkBlue, LocalTime.now());
+            properties.setProperty(COLOR_PROP, "#1A5276");
+            changeColors(properties);
         });
         back.setOnClickListener(view -> {
             final Intent intent = new Intent(Settings.this, MainActivity.class);
