6fce96a40fbb43c61ddaafb56b6c2fda8103448f
rodrigo.albino <Gi_Work_REp&_@10>
2022-10-31 00:35:58 +0000

    Fourth-Assignment: remove deprecated code

--- Projecto1/app/src/main/java/listeners/CanvasListener.java
+++ Projecto1/app/src/main/java/listeners/CanvasListener.java
@@ -1,7 +1,6 @@
 package listeners;
 
 import android.view.GestureDetector;
-import android.view.MotionEvent;
 
 import views.CanvasView;
 
@@ -11,20 +10,6 @@ public class CanvasListener extends GestureDetector.SimpleOnGestureListener {
 
     public void setCanvas(final CanvasView view) {
         this.view = view;
-    }
-
-    ////////SimpleOnGestureListener
-    @Override
-    public void onLongPress(MotionEvent motionEvent) {
-        view.changeBackground();
-
-    }
-
-    /////////OnDoubleTapListener
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-        view.erase();
-        return false;
     }
 
 }
